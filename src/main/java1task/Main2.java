649dfebc8a01223d195204db5552318c7b992e61
Solution <kirya.funker@mail.ru>
2025-01-29 23:33:52 +0300

    4 dz

--- src/main/java1task/Main2.java
+++ src/main/java1task/Main2.java
@@ -1,26 +1,24 @@
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 public class Main2 {
     public static void main(String[] args) {
-        PersonRepository repository = new PersonRepository();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager em = emf.createEntityManager();
 
-        // Добавление
-        PersonEntity person = new PersonEntity();
-        person.setName("Bob");
-        person.setAge(25);
-        repository.addPerson(person);
+        em.getTransaction().begin();
 
+        // Создаем и сохраняем
+        Person person1 = new Person("Alice", 30);
+        Person person2 = new Person("Bob", 25);
 
-        List<PersonEntity> persons = repository.getAllPersons();
-        for (PersonEntity p : persons) {
-            System.out.println("Имя: " + p.getName() + ", Возраст: " + p.getAge());
-        }
+        em.persist(person1);
+        em.persist(person2);
 
-
-        person.setAge(26);
-        repository.updatePerson(person);
-
-
-        repository.deletePerson(person.getId());
+        em.getTransaction().commit();
+        em.close();
+        emf.close();
     }
 }
+
